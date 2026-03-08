719df94309ffd9c841735a606b709811160e2fc6
naingkokolin <naingkokolin007@gmail.com>
2026-03-09 05:22:38 +0630

    added 3 sound effects and resources folder outside kotlin folder

--- Quiz/composeApp/src/jvmMain/kotlin/utils/SoundPlayer.kt
+++ Quiz/composeApp/src/jvmMain/kotlin/utils/SoundPlayer.kt
@@ -1,19 +1,42 @@
 package utils
 
+import java.io.BufferedInputStream
 import javax.sound.sampled.AudioSystem
 
+//object SoundPlayer {
+//
+//    fun play(sound: String) {
+//
+//        val url = SoundPlayer::class.java.getResource("/$sound")
+//
+//        val audioStream = AudioSystem.getAudioInputStream(url)
+//
+//        val clip = AudioSystem.getClip()
+//
+//        clip.open(audioStream)
+//
+//        clip.start()
+//    }
+//}
+
 object SoundPlayer {
-
     fun play(sound: String) {
+        try {
+            // Resource ကို Stream အနေနဲ့ ဖတ်တာက Jar file ထုပ်ပြီးရင်တောင် ပိုအလုပ်လုပ်ပါတယ်
+            val inputStream = javaClass.classLoader.getResourceAsStream(sound)
+                ?: throw Exception("မသိရသေးသော ဖိုင်အမည်: $sound (လမ်းကြောင်းကို ပြန်စစ်ပါ)")
 
-        val url = SoundPlayer::class.java.getResource("/$sound")
+            // BufferedInputStream သုံးတာက အသံထစ်တာကို ကာကွယ်ပေးတယ်
+            val bufferedIn = BufferedInputStream(inputStream)
+            val audioStream = AudioSystem.getAudioInputStream(bufferedIn)
 
-        val audioStream = AudioSystem.getAudioInputStream(url)
+            val clip = AudioSystem.getClip()
+            clip.open(audioStream)
+            clip.start()
 
-        val clip = AudioSystem.getClip()
-
-        clip.open(audioStream)
-
-        clip.start()
+            // အသံပြီးသွားရင် clip ကို ပြန်ပိတ်ချင်ရင် Listener ထည့်လို့ရပါတယ် (Optional)
+        } catch (e: Exception) {
+            println("Sound Player Error: ${e.message}")
+        }
     }
 }
