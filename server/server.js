28a621868aa73b69904d8856a8b14e16dc065abd
naingkokolin <naingkokolin007@gmail.com>
2026-03-08 10:22:08 +0630

    added cors for frontend and backend

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import initDb from "./helpers/db.js";
 import questionRoutes from "./routes/questionRoutes.js";
 import historyRoutes from "./routes/historyRoutes.js";
 import settings from "./routes/settings.js";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 initDb();
